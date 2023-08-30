6dc652c2f316b78eea54b07ef08dbbaea328c8cd
devstoriesplayground <10048477+devstoriesplayground@users.noreply.github.com>
2023-08-30 11:56:15 +0000

    feat: add number datatype

---
+++ datatypes/number.js
@@ -0,0 +1,8 @@
+// Number is can be a integer or float number
+
+console.log(typeof 15); // number
+console.log(typeof Infinity); // number
+console.log(typeof 32.50); // number
+console.log(typeof NaN); // number
+
+console.log(Number.MIN_SAFE_INTEGER, Number.MAX_SAFE_INTEGER); // -9007199254740991, 9007199254740991
