bcce54fe42aca9c419ca9f2d695daa067619c166
devstoriesplayground <10048477+devstoriesplayground@users.noreply.github.com>
2023-09-01 15:24:30 +0000

    feat: add javascript string data type

---
+++ datatypes/string.js
@@ -0,0 +1,7 @@
+// It is a primitive data type that is used to work with texts. It can be surrounded my single or double quote or backticks are generally used when you need to include variables or expressions into a string.
+
+console.log('Hello World!'); // Hello World!
+console.log(typeof 'Hello'); // string
+
+const world = 'World';
+console.log(`Hello ${world}`); // Hello World!
