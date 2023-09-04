4176c69cc65325a62f347a8fc24f6dca9663c30e
devstoriesplayground <10048477+devstoriesplayground@users.noreply.github.com>
2023-09-04 14:10:51 +0000

    feat: add javascript string concat

---
+++ datatypes/stringMethod/concat.js
@@ -0,0 +1,9 @@
+
+
+const text1 = 'hello';
+const text2 = 'world';
+const text3 = '     Hello World;
+  
+// concatenating two strings
+const result1 = text1.concat(' ', text2);
+console.log(result1); // "hello world"
