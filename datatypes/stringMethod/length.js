45f32e9ed79021b0936ea9544eda0acbd318946e
CDA <10048477+devstoriesplayground@users.noreply.github.com>
2023-09-05 16:43:55 +0800

    feat: add javascript string length method

---
+++ datatypes/stringMethod/length.js
@@ -0,0 +1,12 @@
+// The length property returns the number of characters in a string.
+
+// defining a string
+let sentence = "I love Javasript.";
+
+// returns number of characters in the sentence string
+let len = sentence.length;
+
+console.log(len);
+
+// Output:
+// 17
