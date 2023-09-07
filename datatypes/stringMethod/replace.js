8de901d56bff1eacf07eea8cdb14d04a6e16dc22
CDA <10048477+devstoriesplayground@users.noreply.github.com>
2023-09-07 14:53:03 +0800

    feat: add replace, search, split string method

---
+++ datatypes/stringMethod/replace.js
@@ -0,0 +1,9 @@
+// The replace() method returns a new string with the specified string/regex replaced.
+
+const message = "ball bat";
+
+// replace the first b with c
+let result = message.replace('b', 'c');
+console.log(result);
+
+// Output: call bat
