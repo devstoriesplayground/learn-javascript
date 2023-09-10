7ccda2fc77ac4224956e47d78fa407ecb8a8d063
devstoriesplayground <10048477+devstoriesplayground@users.noreply.github.com>
2023-09-10 11:39:18 +0000

    feat: add null data type

---
+++ datatypes/null.js
@@ -0,0 +1,8 @@
+// In JavaScript, null is a special value that represents empty or unknown value.
+
+// that typeof returned "object" for the null type. 
+// This is a known issue in JavaScript since its first release.
+console.log(typeof null); // object
+
+// you can use null to assign 'unknown' or 'empty' value to a variable
+let a = null;
