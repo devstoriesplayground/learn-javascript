3dd78ed4bd57145b02928c1a9a55136da7c60a12
CDA <10048477+devstoriesplayground@users.noreply.github.com>
2023-09-13 16:56:24 +0800

    feat: add bigInt and undefined data type

---
+++ datatypes/bigInt.js
@@ -0,0 +1,14 @@
+// A BigInt number is created by appending n to the end of an integer.
+
+// BigInt value
+const value1 = 900719925124740998n;
+
+// Adding two big integers
+const result1 = value1 + 1n;
+console.log(result1); // "900719925124740999n"
+
+const value2 = 900719925124740998n;
+
+// Error! BitInt and number cannot be added
+const result2 = value2 + 1; 
+console.log(result2); 
