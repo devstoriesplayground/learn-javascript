95918d4495ac4e75e653df6f4e8bb11132d5ae46
CDA <10048477+devstoriesplayground@users.noreply.github.com>
2023-09-22 16:58:39 +0800

    feat: add while and break in js

---
+++ flowcontrol/while.js
@@ -0,0 +1,11 @@
+// In programming, loops are used to repeat a block of code. For example, if you want to show a message 100 times, then you can use a loop.
+
+// program to display numbers from 1 to 5
+// initialize the variable
+let i = 1, n = 5;
+
+// while loop from i = 1 to 5
+while (i <= n) {
+    console.log(i);
+    i += 1;
+}
